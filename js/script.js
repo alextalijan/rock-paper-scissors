5a29a9bfc1c087c0b1a89863c6825fe186b5c371
alextalijan <acatalijan@icloud.com>
2025-04-24 19:25:35 -0500

    feat: write full logic for random computer moves
    In external js file, a random number of 3 is selected
    and based on it, a move is returned.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello, Alex")
+// A function that randomly returns rock, paper or scissors
+function getComputerChoice() {
+    // Choose a random number between 1 and 3
+    randomMoveNumber = Math.floor(Math.random() * 3) + 1;
+
+    // Depending on the number, choose a particular possible move
+    switch(randomMoveNumber) {
+        case 1:
+            return "rock";
+        
+        case 2:
+            return "paper";
+        
+        case 3:
+            return "scissors";
+    }
+}
