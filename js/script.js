d4eb90f48c5c06d0b8eca067ee213d0474a34b10
alextalijan <acatalijan@icloud.com>
2025-04-24 20:03:49 -0500

    feat: write logic for a single round of the game
    In a function playRound, a whole round is defined
    incrementing the score of the winner and printing
    a proper message.

--- js/script.js
+++ js/script.js
@@ -28,14 +28,52 @@ function getHumanChoice() {
 
 // Function that runs one round of the game
 function playRound(humanChoice, computerChoice) {
-    // Capture computer and human moves in variables
-    const computerMove = getComputerChoice();
-    const humanMove = getHumanChoice();
+    // If the two moves are same, announce a tie
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie. You both chose ${humanChoice}.`);
+    } // Else check the relationship between moves and announce the winner
+    else {
+        switch(humanChoice) {
+            case "rock":
+                if (computerChoice === "paper") {
+                    console.log("You lose! Paper beats Rock.");
+                    computerScore++;
+                } else {
+                    console.log("You win! Rock beats Scissors.");
+                    humanScore++;
+                }
+                break;
+
+            case "paper":
+                if (computerChoice === "rock") {
+                    console.log("You win! Paper beats Rock.");
+                    humanScore++;
+                } else {
+                    console.log("You lose! Scissors beat Paper.");
+                    computerScore++;
+                }
+                break;
+
+            case "scissors":
+                if (computerChoice === "paper") {
+                    console.log("You win! Scissors beat Paper.");
+                    humanScore++;
+                } else {
+                    console.log("You lose! Rock beats Scissors.");
+                    computerScore++;
+                }
+                break;
+        }
+    }
 }
 
 // Declare variables to keep track of user and computer score
 let humanScore, computerScore = 0;
 
+// Capture computer and human moves in variables
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice().toLowerCase();
+
 // Play a round
-playRound(humanMove, computerMove);
+playRound(humanChoice, computerChoice);
 
